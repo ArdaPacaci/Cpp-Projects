b1bbf73b0cf369bebfa8bf985f8d270313600341
Arda Paçacı <ardapacaci2434@gmail.com>
2026-03-06 13:09:18 +0300

    Add files via upload

---
+++ FactorialCalculator.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int factorial(int);
+
+int main(){
+	int result,number;
+	cout<<"enter a number:";
+	cin>>number;
+	result=factorial(number);
+	cout<<number<<"!="<<result<<"\n";
+	return 0;
+}
+int factorial(int x){
+	if(x==0)
+	return 1;
+	
+	else
+	    return x*factorial(x-1);
+}
